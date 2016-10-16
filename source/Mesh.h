78846d2e23c54af8c8c1a11bfc2429ff5836ac8b
Diego Gonzalez <drg4@williams.edu>
2016-10-16 20:39:07 +0000

    added textures, fixed the branch section bug, and changed the file generation from two obj files to just one

--- source/Mesh.h
+++ source/Mesh.h
@@ -6,18 +6,30 @@
 
   Data structure used to hold information about a mesh as it is constructed.
  */
+class Face {
+    public:
+        Face(const String texture);
+        void increment(int pts);
+        String texture;
+        SmallArray<int,3> points;
+        SmallArray<int,3> txtPoints;
+
+};
 
 class Mesh {
 	protected:
 		String filename;
 		std::vector<Point3> vertices;
-		std::vector<SmallArray<int, 3>> faces;
+		std::vector<Face> faces;
 	public:
 		void addVertex(float x, float y, float z);
         void addVertex(Point3 vertex);
-		void addFace(int f1, int f2, int f3);
+		void addFace(int f1, int f2, int f3, int t1, int t2, int t3, String type);
+        void addFace(Face);
+        void addMesh(Mesh other);
 		Mesh(const String& fn);
 		void toOFF();
         void toOBJ();
         int numVertices();
 };
+
